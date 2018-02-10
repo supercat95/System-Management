cac818e2e0472b19d161dfab56fb0bf11709ce8d
supercat95 <joybates@joys-new-mbp.home>
2018-02-10 17:08:00 -0600

    Initial Commit

---
+++ System Management/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  System Management
+//
+//  Created by Joy Bates on 2/10/18.
+//  Copyright © 2018 Joy Bates. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
